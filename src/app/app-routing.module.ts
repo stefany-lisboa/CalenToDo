2ad9cce236e25ae80457b3311483da400c5754c1
Stefany Lisboa <stefany22lisboa@gmail.com>
2024-02-22 03:07:29 -0300

    created calendar component with partial rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TaskListComponent } from './task-list/task-list.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 const routes: Routes = [
-  { path: '', component: TaskListComponent }
+  { path: '', component: CalendarComponent },
+  { path: 'task', component: TaskListComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
